ab606069c0201bf29de2b86d4a81ea1c9cdaa3c1
jishankai <jishankai@gmail.com>
2023-03-29 11:54:25 +0800

    add UTC

--- cron/price.py
+++ cron/price.py
@@ -32,7 +32,7 @@ def get_prices():
 
     text = "🏷️ Spot Prices\n\n"
     text += f'<i>BTC price: ${btc_price:.2f}</i>\n<i>ETH price: ${eth_price:.2f}</i>\n\n'
-    text += f'<i>{now.strftime("%Y-%m-%d %H:%M")}</i>'
+    text += f'<i>{now.strftime("%Y-%m-%d %H:%M")} UTC</i>'
     
     bot.send_message(chat_id=config_yaml["group_chat_id"], text=text, parse_mode=telegram.ParseMode.HTML)
 
